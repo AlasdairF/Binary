4fd3349e020b978ca49049a267e62f99d95fc82a
AlasdairF <AlasdairF@users.noreply.github.com>
2014-08-19 20:55:35 +0700

    bugfix

--- bin.go
+++ bin.go
@@ -1,31 +1,31 @@
 package bin
 
-func decode16(b []byte) uint16 {
+func Decode16(b []byte) uint16 {
 	return uint16(b[0]) | uint16(b[1])<<8
 }
 
-func encode16(b []byte, v uint16) {
+func Encode16(b []byte, v uint16) {
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 }
 
-func decode32(b []byte) uint32 {
+func Decode32(b []byte) uint32 {
 	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
 }
 
-func encode32(b []byte, v uint32) {
+func Encode32(b []byte, v uint32) {
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
 	b[3] = byte(v >> 24)
 }
 
-func decode64(b []byte) uint64 {
+func Decode64(b []byte) uint64 {
 	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
 		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
 }
 
-func encode64(b []byte, v uint64) {
+func Encode64(b []byte, v uint64) {
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
